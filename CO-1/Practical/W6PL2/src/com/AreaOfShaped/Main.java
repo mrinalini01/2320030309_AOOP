4c68e99f944d6641a6f3a463646e5981167dfe43
BANALA MRINALINI <itsmrinalini.b@gmail.com>
2024-09-09 23:08:34 +0530

    Add files via upload

---
+++ CO-1/Practical/W6PL2/src/com/AreaOfShaped/Main.java
@@ -0,0 +1,12 @@
+package com.AreaOfShaped;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle(5, 3);
+        Shape circle = new Circle(4);
+
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+        System.out.println("Area of Circle: " + circle.calculateArea());
+    }
+}
+
---
+++ CO-1/Practical/W6PL2/src/com/AreaOfShaped/Shape.java
@@ -0,0 +1,5 @@
+package com.AreaOfShaped;
+
+abstract class Shape {
+    public abstract double calculateArea();
+}
